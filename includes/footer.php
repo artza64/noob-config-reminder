e1e34143b532ad0e4e0fb40e990e0354461fbc39
artza64 <36849069+artza64@users.noreply.github.com>
2018-05-21 00:39:01 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer id="footer" class="wrapper">
+		<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+		<p>Try awsome tool for desgners symu.co</p>
+	</footer>
+
+	
+</html>
---
+++ includes/pages.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main class="wrapper">
+	<section class="content">
+		<h1 class="title-content"><?= get_the_title(); ?></h1>
+	</section>
+</main>
+
+<?php get_footer(); ?>
